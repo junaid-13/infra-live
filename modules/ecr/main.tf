2b1f254a2f0f11026a897898b4258d761c588187
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-09-03 15:37:52 +0530

    written the code for main.tf file in the ecr module.

---
+++ modules/ecr/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = { source = "hashicorp/aws", version = "~> 5.55" }
+  }
+}
+provider "aws" {}
+
+module "ecr" {
+    source = "terraform-aws-modules/ecr/aws"
+    version = "~>2.4"
+
+    for_each = toset(var.repositories)
+    repository_name = each.value
+    repository_image_tag_mutability = "MUTABLE"
+    repository_image_scan_on_push = true
+
+    tags = merge(var.tags, {"Service" = each.value})
+}
