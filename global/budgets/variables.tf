e413abea1708d97682cbba8c8d4a9f40850fab31
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-09-03 15:22:30 +0530

    Written the variables.tf file.

---
+++ global/budgets/variables.tf
@@ -0,0 +1,14 @@
+
+variable "emails" {
+  description = "Email addresses to notify on budget alerts"
+  type = list(string)
+}
+
+variable "amounts" {
+  description = "Monthly budget caps per env"
+  type = object({
+    dev = number
+    stage = number
+    prod = number
+  })
+}
