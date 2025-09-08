4c319611c78bbb2063b7ef7f2b50fdfc2e6e7edc
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-09-08 14:00:30 +0530

    Fixed the error related to the ecr url syntax in output file.

--- envs/dev/outputs.tf
+++ envs/dev/outputs.tf
@@ -1,4 +1,4 @@
 output "cluster_name"   { value = module.eks.cluster_name }
-output "ecr_repo_urls"  { value = module.ecr.repo_urls }
+output "ecr_repo_urls"  { value = module.ecr.repo_url }
 output "vpc_id"         { value = module.network.vpc_id }
 output "private_subnets"{ value = module.network.private_subnets }
