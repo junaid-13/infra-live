6b6b74bb22b1a8c300e4d85afb848784bb66c4b6
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-09-10 17:15:56 +0530

    Fixing the vpc id selected related error.

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
     aws = {
-        source  = "hashicorp/aws"
-        version = "~> 5.55"
+      source  = "hashicorp/aws"
+      version = "~> 5.55"
     }
     kubernetes = {
-        source  = "hashicorp/kubernetes"
-        version = "~> 2.32"
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.32"
     }
     helm = {
-        source  = "hashicorp/helm"
-        version = "~> 2.14"
+      source  = "hashicorp/helm"
+      version = "~> 2.14"
     }
   }
 }
@@ -21,36 +21,31 @@ provider "aws" {}
 # kubernetes/helm providers will be configured by caller using cluster outputs.
 
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
-  version = "~> 20.24"
-  cluster_name = var.cluster_name
-  cluster_version = var.ver
+  source                         = "terraform-aws-modules/eks/aws"
+  version                        = "~> 20.24"
+  cluster_name                   = var.cluster_name
+  cluster_version                = var.ver
   cluster_endpoint_public_access = true
-  enable_irsa = true
+  enable_irsa                    = true
 
-  vpc_id = data.aws_vpc.selected.id
+  vpc_id     = data.aws_vpc.id
   subnet_ids = var.subnet_ids
-  
+
   eks_managed_node_groups = {
     dev_spot = {
-        ami_type = "AL2_x86_64"
-        instance_types = var.instance_types
-        capacity_type = var.capacity_type
-        desired_size = var.desired_size
-        min_size = var.min_size
-        max_size = var.max_size
-        labels = {
-            "finops.io/env" = "dev"
-            "workload" = "general"
-        }
-        tags = var.tags
+      ami_type       = "AL2_x86_64"
+      instance_types = var.instance_types
+      capacity_type  = var.capacity_type
+      desired_size   = var.desired_size
+      min_size       = var.min_size
+      max_size       = var.max_size
+      labels = {
+        "finops.io/env" = "dev"
+        "workload"      = "general"
+      }
+      tags = var.tags
     }
   }
-    tags = var.tags
-  }
+  tags = var.tags
+}
 
-
-  data "aws_vpc" "selected" {
-    id = regex("vpc-.+",var.tags["VpcId"] != null ? var.tags["VpcId"] : "")
-    # For simplicity, caller will not set VpcId; module will infer via subnets in practice.
-  }
--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -3,6 +3,14 @@ variable "cluster_name" {
   type = string
 }
 
+variable "vpc_id" {
+  type = string
+}
+
+variable "ver" {
+  type = string
+}
+
 variable "ver" {
   type = string
 }
@@ -12,31 +20,31 @@ variable "subnet_ids" {
 }
 
 variable "tags" {
-  type = map(string)
+  type    = map(string)
   default = {}
 }
 
 variable "instance_types" {
-  type = list(string)
+  type    = list(string)
   default = ["t3.medium"]
 }
 
 variable "capacity_type" {
-  type = string
+  type    = string
   default = "SPOT"
 }
 
 variable "desired_size" {
-  type = number
+  type    = number
   default = 2
 }
 
 variable "min_size" {
-  type = number
+  type    = number
   default = 1
 }
 
 variable "max_size" {
-  type = number
-  default = 4 
+  type    = number
+  default = 4
 }
